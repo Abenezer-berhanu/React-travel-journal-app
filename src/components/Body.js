7abcff34c80433b4ae4a8351b9b32ea6f9a80ebc
Abenezer Berhanu <abenezerberhanu40@gmail.com>
2023-04-05 19:43:55 -0400

    conditional statments add to props

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,26 +2,32 @@ import React from "react";
 import "../Styles.css";
 
 export default function body(props) {
+          const condition = props.Data.location || props.Data.googleMapsUrl
+          const discriptionCondition =
+            props.Data.title ||
+            props.Data.startDate ||
+            props.Data.endDate ||
+            props.Data.description
   return (
     <div className="Body--container">
       <div className="Body--images">
         <img src={props.Data.image} alt="moutain--image" />
       </div>
       <div className="Place--description">
-        <span className="location--country">
+        {condition && <span className="location--country">
           <img src="Images/Location.png" alt="" />
           <p>{props.Data.location}</p>
           <a href={props.Data.googleMapsUrl} target="_blank">
             View on google maps
           </a>
-        </span>
-        <span className="Body--description">
+        </span>}
+        {discriptionCondition && <span className="Body--description">
           <p className="Mountain--name">{props.Data.title}</p>
           <p className="Dates">
             {props.Data.startDate} {props.Data.endDate}
           </p>
           <p className="Place--descripton">{props.Data.description}</p>
-        </span>
+        </span>}
       </div>
     </div>
   );
